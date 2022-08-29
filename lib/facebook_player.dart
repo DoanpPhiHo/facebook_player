d7b00b1fbadbb7dd643a7bf6c0774a19bc9936d3
DoanHo <doanho.it.dev@gmail.com>
2022-08-29 15:51:13 +0700

    init-code

---
+++ lib/facebook_player.dart
@@ -0,0 +1,48 @@
+library facebook_player;
+
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+class FacebookVideoPlayer extends StatefulWidget {
+  const FacebookVideoPlayer({
+    Key? key,
+    required this.urlFb,
+  }) : super(key: key);
+
+  final String urlFb;
+
+  @override
+  State<FacebookVideoPlayer> createState() => _FacebookVideoPlayerState();
+}
+
+class _FacebookVideoPlayerState extends State<FacebookVideoPlayer> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: AspectRatio(
+            aspectRatio: 16 / 9,
+            child: InAppWebView(
+              initialOptions: InAppWebViewGroupOptions(
+                ios: IOSInAppWebViewOptions(
+                  allowsInlineMediaPlayback: true,
+                ),
+              ),
+              initialUrlRequest: URLRequest(
+                url: Uri.parse(
+                    'https://www.facebook.com/v3.2/plugins/video.php?allowfullscreen=false&href=${widget.urlFb}'),
+              ),
+              onConsoleMessage: (controller, consoleMessage) {
+                log(consoleMessage.message);
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
